0abe6272227b406894841750bc915e79677eab54
Looofeee <Looofeee@mail.ru>
2017-09-29 01:30:34 +0300

    V1

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,29 @@
 class TicTacToe {
     constructor() {
-
+        this.player = "x";
+        this.matrix = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.player;   
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    nextTurn(rowIndex, colmnIndex) {
+        this.rowIndex = rowIndex;
+        this.colmnIndex = colmnIndex;
+        if(this.getFieldValue(this.rowIndex, this.colmnIndex) === null) {
+            this.matrix[this.rowIndex][this.colmnIndex] = this.player;
+            if(this.player === "x") {
+                this.player = "o";
+            }
+            else if (this.player === "o"){
+                this.player = "x";
+            }
+        } 
     }
 
     isFinished() {
@@ -28,7 +43,12 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+        if(this.matrix[this.rowIndex][this.colIndex] === null){
+            return null;
+        }
+        
     }
 }
 
