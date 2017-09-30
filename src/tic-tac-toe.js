7ed8e7180b23b2b7f161d7f0c87ef785a45874b0
Looofeee <Looofeee@mail.ru>
2017-09-30 18:24:36 +0300

    V2 - all

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,11 +1,13 @@
 class TicTacToe {
     constructor() {
         this.player = "x";
-        this.matrix = [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null]
-        ];
+        this.matrix = [];
+        for (var i = 0; i < 3; i++) {
+            this.matrix[i] = [];
+            for (var j = 0; j < 3; j++) {
+                this.matrix[i][j] = null;
+            }
+        }
     }
 
     getCurrentPlayerSymbol() {
@@ -27,19 +29,97 @@ class TicTacToe {
     }
 
     isFinished() {
-
+        if(this.getWinner() === 'x' || this.getWinner() === 'o' || this.isDraw() === true) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     getWinner() {
 
+    //////
+        var winner = 0;
+        var p = 0;
+        var symbol = 'x';
+        while(p!=2) {
+                                for (var i = 0; i < 3; i++) {
+                        for (var j = 0; j < 3; j++) {
+                            if(this.matrix[i][j] === symbol) {
+                                winner++;
+                                if(winner === 3){
+                                    return symbol;
+                                }
+                        }
+                    }
+                    winner = 0;
+                    }
+                    winner = 0;
+
+
+                    for (var i = 0; i < 3; i++) {
+                        for (var j = 0; j < 3; j++) {
+                            if(this.matrix[j][i] === symbol) {
+                                winner++;
+                                if(winner === 3){
+                                    return symbol;
+                                }
+                        }
+                    }
+                    winner = 0;
+                    }
+                    winner = 0;
+
+                    for (var i = 0; i < 3; i++) {
+                        var j = i;
+                        if(this.matrix[j][i] === symbol) {
+                                winner++;
+                                if(winner === 3){
+                                    return symbol;
+                                }
+                        }
+                    }
+                    winner = 0;
+                    
+                    for (var i = 0; i < 3; i++) {
+                        var j = 2 - i;
+                        if(this.matrix[j][i] === symbol) {
+                                winner++;
+                                if(winner === 3){
+                                    return symbol;
+                                }
+                        }
+                    }
+                    winner =0;
+                    symbol = 'o';
+                p++;
+        }
+        return null;
+                ///
     }
 
     noMoreTurns() {
-
+        var score = 0;
+        for(var i = 0; i < 3; i++){
+            for(var j = 0; j < 3; j++){
+                if(this.matrix[i][j] !== null){
+                    score++;
+                } ;
+            } ;
+        } ;
+        if(score === 9) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     isDraw() {
-
+        if(this.noMoreTurns() === true && this.getWinner() === null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     getFieldValue(rowIndex, colIndex) {
@@ -47,6 +127,8 @@ class TicTacToe {
         this.colIndex = colIndex;
         if(this.matrix[this.rowIndex][this.colIndex] === null){
             return null;
+        } else {
+            return this.matrix[this.rowIndex][this.colIndex];
         }
         
     }
